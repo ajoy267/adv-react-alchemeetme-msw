c0f49d19df9f817970b1b56618167266de070a67
ajoy267 <andrew.joy12@hotmail.com>
2022-01-26 13:56:45 -0800

    ran npm i and imported rest and setupServer

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,7 @@
 // 🚨🚨 https://mswjs.io/docs/ 🚨🚨
 
 import { screen, render } from '@testing-library/react'
+import { rest, setupServer } from 'msw'
 // 🚨
 // import rest
 // import setupServer
@@ -11,7 +12,7 @@ const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: '',
+  name: 'Vonta',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
